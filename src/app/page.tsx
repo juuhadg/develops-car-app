935322dd2232d8b8edf190a99ce930f68facfba1
juuhadg <juku.jj@gmail.com>
2024-08-19 17:25:11 -0300

    fix: formatting fixes

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,35 +1,42 @@
-"use client";
-import { Menu, MenuButton, MenuItem, MenuItems } from "@headlessui/react";
-import { ArrowRightIcon, ChevronDownIcon, ArrowPathIcon  } from "@heroicons/react/16/solid";
-import Link from "next/link";
-import { useEffect, useState } from "react";
+'use client';
+import { Menu, MenuButton, MenuItem, MenuItems } from '@headlessui/react';
+import {
+  ArrowRightIcon,
+  ChevronDownIcon,
+  ArrowPathIcon,
+} from '@heroicons/react/16/solid';
+import Link from 'next/link';
+import { useEffect, useState } from 'react';
 export default function Home() {
-
   const [carTypes, setCarTypes] = useState([]);
   const [years, setYears] = useState<number[]>([]);
   const [loading, setLoading] = useState(true);
-  const [error, setError] : any = useState(null);
+  const [error, setError]: any = useState(null);
 
-  const [selectedCarType, setSelectedCarType] = useState<{ name: string; id: number } | null>(null);
+  const [selectedCarType, setSelectedCarType] = useState<{
+    name: string;
+    id: number;
+  } | null>(null);
   const [selectedYear, setSelectedYear] = useState(null);
-
-
 
   useEffect(() => {
     const fetchCarTypes = async () => {
       try {
-        
-        const response = await fetch('https://vpic.nhtsa.dot.gov/api/vehicles/GetMakesForVehicleType/car?format=json');
+        const response = await fetch(
+          'https://vpic.nhtsa.dot.gov/api/vehicles/GetMakesForVehicleType/car?format=json'
+        );
         if (!response.ok) {
           throw new Error('Car Types Fetch Error');
         }
         const result = await response.json();
-        
-        const sortedTypes = result.Results.sort((a: { MakeName: string; }, b: { MakeName: string; }) => a.MakeName.localeCompare(b.MakeName));
+
+        const sortedTypes = result.Results.sort(
+          (a: { MakeName: string }, b: { MakeName: string }) =>
+            a.MakeName.localeCompare(b.MakeName)
+        );
 
         setCarTypes(sortedTypes);
       } catch (error) {
-        
         setError(error);
       } finally {
         setLoading(false);
@@ -43,92 +50,100 @@ export default function Home() {
         (_, index) => currentYear - index
       );
       setYears(yearArray);
-    }
+    };
 
     fetchCarTypes();
-    getDynamicYears()
-  }, []); 
+    getDynamicYears();
+  }, []);
 
-  
   if (error) return <p>Error: {error.message}</p>;
 
   const isButtonDisabled = !selectedCarType || !selectedYear;
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24 bg-pageBg ">
       <div className="bg-white shadow-searchBox p-10 rounded-md ">
-        <p className="text-3xl font-bold text-black mb-5 text-center">Search For your Car</p>
+        <p className="text-3xl font-bold text-black mb-5 text-center">
+          Search For your Car
+        </p>
 
         <div className="flex items-center justify-left gap-5">
+          <Menu as="div" className="relative inline-block text-left w-72">
+            {loading ? (
+              <p className="text-center text-black">Loading...</p>
+            ) : (
+              <>
+                <MenuButton className="inline-flex justify-center w-full items-center rounded-md border border-gray-300 shadow-sm px-4 py-2 bg-white text-sm font-medium text-gray-700 hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-gray-100 focus:ring-indigo-500">
+                  {selectedCarType ? selectedCarType.name : 'Car Type'}
+                  <ChevronDownIcon className="size-4 fill-black/60" />
+                </MenuButton>
 
-    
-                <Menu as="div" className="relative inline-block text-left w-72">
-                  {loading ? <p className="text-center text-black">Loading...</p> : (
-                    <>
-                     <MenuButton className="inline-flex justify-center w-full items-center rounded-md border border-gray-300 shadow-sm px-4 py-2 bg-white text-sm font-medium text-gray-700 hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-gray-100 focus:ring-indigo-500">
-                     {selectedCarType ?  selectedCarType.name : 'Car Type'}	
-                     <ChevronDownIcon className="size-4 fill-black/60" />
-           
-                     </MenuButton>
-           
-                     <MenuItems transition className="origin-top-right absolute right-0 mt-2 w-72 rounded-md shadow-lg bg-white ring-1 ring-black ring-opacity-5 focus:outline-none overflow-y-auto h-60">
-                     
-                    
-                     {carTypes.map((carType: any) => (
-                       <MenuItem key={carType.MakeId}>
-                       <button onClick={() => {selectedCarType === carType.MakeId ? setSelectedCarType(null) : setSelectedCarType({name: carType.MakeName, id: carType.MakeId})}} className="group flex w-full items-center gap-2 rounded-lg py-1.5 px-3 data-[focus]:bg-black/10 text-black">
+                <MenuItems
+                  transition
+                  className="origin-top-right absolute right-0 mt-2 w-72 rounded-md shadow-lg bg-white ring-1 ring-black ring-opacity-5 focus:outline-none overflow-y-auto h-60"
+                >
+                  {carTypes.map((carType: any) => (
+                    <MenuItem key={carType.MakeId}>
+                      <button
+                        onClick={() => {
+                          selectedCarType === carType.MakeId
+                            ? setSelectedCarType(null)
+                            : setSelectedCarType({
+                                name: carType.MakeName,
+                                id: carType.MakeId,
+                              });
+                        }}
+                        className="group flex w-full items-center gap-2 rounded-lg py-1.5 px-3 data-[focus]:bg-black/10 text-black"
+                      >
                         {carType.MakeName}
-                        </button>
-                      </MenuItem>
-                     ))}
-                       
-                 
-                   </MenuItems>
-                   </>
-                  )}
-               
-                </Menu>
-        
+                      </button>
+                    </MenuItem>
+                  ))}
+                </MenuItems>
+              </>
+            )}
+          </Menu>
 
           <Menu as="div" className="relative inline-block text-left">
-          <MenuButton className="inline-flex justify-center w-full items-center rounded-md border border-gray-300 shadow-sm px-4 py-2 bg-white text-sm font-medium text-gray-700 hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-gray-100 focus:ring-indigo-500">
-          {selectedYear ? selectedYear : 'Year'}	
-          <ChevronDownIcon className="size-4 fill-black/60" />
+            <MenuButton className="inline-flex justify-center w-full items-center rounded-md border border-gray-300 shadow-sm px-4 py-2 bg-white text-sm font-medium text-gray-700 hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-gray-100 focus:ring-indigo-500">
+              {selectedYear ? selectedYear : 'Year'}
+              <ChevronDownIcon className="size-4 fill-black/60" />
+            </MenuButton>
 
-          </MenuButton>
-
-          <MenuItems transition className="origin-top-right absolute right-0 mt-2 w-72 rounded-md shadow-lg bg-white ring-1 ring-black ring-opacity-5 focus:outline-none overflow-y-auto h-60">
-          
-         
-          {years.map((year: any) => (
-            <MenuItem key={year}>
-            <button onClick={() => {selectedYear === year ? setSelectedYear(null) : setSelectedYear(year)}} className="group flex w-full items-center gap-2 rounded-lg py-1.5 px-3 data-[focus]:bg-black/10 text-black">
-             {year}
-             </button>
-           </MenuItem>
-          ))}
-            
-      
-        </MenuItems>
+            <MenuItems
+              transition
+              className="origin-top-right absolute right-0 mt-2 w-72 rounded-md shadow-lg bg-white ring-1 ring-black ring-opacity-5 focus:outline-none overflow-y-auto h-60"
+            >
+              {years.map((year: any) => (
+                <MenuItem key={year}>
+                  <button
+                    onClick={() => {
+                      selectedYear === year
+                        ? setSelectedYear(null)
+                        : setSelectedYear(year);
+                    }}
+                    className="group flex w-full items-center gap-2 rounded-lg py-1.5 px-3 data-[focus]:bg-black/10 text-black"
+                  >
+                    {year}
+                  </button>
+                </MenuItem>
+              ))}
+            </MenuItems>
           </Menu>
 
           <div>
-        <Link
-          href={`/result/${selectedCarType?.id}/${selectedYear}`}
-          passHref
-          className={`inline-flex items-center gap-2 w-40 justify-center bg-red-600 p-2 rounded-lg text-white hover:bg-red-700 ${isButtonDisabled ? 'opacity-30 cursor-not-allowed' : ''}`}
-          aria-disabled={isButtonDisabled}
-          style={{ pointerEvents: isButtonDisabled ? 'none' : 'auto' }}
-        >
-        
-            Next
-            <ArrowRightIcon className="w-4 h-4 text-white" />
-          
-        </Link>
-      </div>
+            <Link
+              href={`/result/${selectedCarType?.id}/${selectedYear}`}
+              passHref
+              className={`inline-flex items-center gap-2 w-40 justify-center bg-red-600 p-2 rounded-lg text-white hover:bg-red-700 ${isButtonDisabled ? 'opacity-30 cursor-not-allowed' : ''}`}
+              aria-disabled={isButtonDisabled}
+              style={{ pointerEvents: isButtonDisabled ? 'none' : 'auto' }}
+            >
+              Next
+              <ArrowRightIcon className="w-4 h-4 text-white" />
+            </Link>
+          </div>
         </div>
-
       </div>
     </main>
   );
 }
-
--- src/components/carInformation.tsx
+++ src/components/carInformation.tsx
@@ -1,5 +1,5 @@
-"use client";
-import { useEffect, useState } from "react";
+'use client';
+import { useEffect, useState } from 'react';
 
 interface VehicleData {
   Make_ID: string;
@@ -20,7 +20,9 @@ export default function CarInformation({ id, year }: CarInformationProps) {
   useEffect(() => {
     const fetchVehicleData = async () => {
       try {
-        const response = await fetch(`https://vpic.nhtsa.dot.gov/api/vehicles/GetModelsForMakeIdYear/makeId/${id}/modelyear/${year}?format=json`);
+        const response = await fetch(
+          `https://vpic.nhtsa.dot.gov/api/vehicles/GetModelsForMakeIdYear/makeId/${id}/modelyear/${year}?format=json`
+        );
         if (!response.ok) {
           throw new Error('Failed to fetch vehicle data');
         }
@@ -34,20 +36,21 @@ export default function CarInformation({ id, year }: CarInformationProps) {
     fetchVehicleData();
   }, [id, year]);
 
-
   if (error) return <div>Error: {error}</div>;
   return (
     <>
-      <h1 className="text-center font-extrabold text-5xl mb-5">{`${vehicleData.length > 0 ? vehicleData[0].Make_Name : 'No Cars Found' } Cars From ${year}`}</h1>
+      <h1 className="text-center font-extrabold text-5xl mb-5">{`${vehicleData.length > 0 ? vehicleData[0].Make_Name : 'No Cars Found'} Cars From ${year}`}</h1>
       <div className="flex gap-5 flex-wrap items-center justify-center">
         {vehicleData.length === 0 && <p>No cars found.</p>}
         {vehicleData.map((item) => (
-          <div key={item.Model_ID} className="mb-4 bg-pageBg p-5 w-1/4 rounded-xl h-1/5">
+          <div
+            key={item.Model_ID}
+            className="mb-4 bg-pageBg p-5 w-1/4 rounded-xl h-1/5"
+          >
             <p className="text-2xl font-bold">{`Make Id: ${item.Make_ID}`}</p>
             <p className="text-2xl font-bold">{`Make Name: ${item.Make_Name}`}</p>
             <p className="text-2xl font-bold">{`Model Id: ${item.Model_ID}`}</p>
             <p className="text-2xl font-bold">{` Name: ${item.Model_Name}`}</p>
-
           </div>
         ))}
       </div>
